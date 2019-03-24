1459d621f0dc1d038564c3a2c2b7d7af8536e39b
Sandeep <sandeep.lakdawala@gmail.com>
2019-03-24 16:53:23 +0530

    DP-UglyNumbers

---
+++ src/main/java/com/sherlock/learn/dynamic/programming/UglyNumbersDP.java
@@ -0,0 +1,54 @@
+package com.sherlock.learn.dynamic.programming;
+
+import com.google.common.base.Stopwatch;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+public class UglyNumbersDP {
+
+    public static void main(String[] args) {
+        Stopwatch sw = Stopwatch.createStarted();
+        int n = 1500;
+        System.out.println(getNextUglyNo(n));
+        sw.stop();
+        System.out.println("Time:"+sw.elapsed(MILLISECONDS));
+    }
+
+    private static int getNextUglyNo(int n) {
+
+        int ugly[] = new int[n];  // To store ugly numbers
+        int i2 = 0, i3 = 0, i5 = 0;
+        int next_multiple_of_2 = 2;
+        int next_multiple_of_3 = 3;
+        int next_multiple_of_5 = 5;
+        int next_ugly_no = 1;
+
+        ugly[0] = 1;
+
+        for(int i = 1; i < n; i++)
+        {
+            next_ugly_no = Math.min(next_multiple_of_2,
+                    Math.min(next_multiple_of_3,
+                            next_multiple_of_5));
+
+            ugly[i] = next_ugly_no;
+            if (next_ugly_no == next_multiple_of_2)
+            {
+                i2 = i2+1;
+                next_multiple_of_2 = ugly[i2]*2;
+            }
+            if (next_ugly_no == next_multiple_of_3)
+            {
+                i3 = i3+1;
+                next_multiple_of_3 = ugly[i3]*3;
+            }
+            if (next_ugly_no == next_multiple_of_5)
+            {
+                i5 = i5+1;
+                next_multiple_of_5 = ugly[i5]*5;
+            }
+        } /*End of for loop (i=1; i<n; i++) */
+        return next_ugly_no;
+
+    }
+}
---
+++ src/main/java/com/sherlock/learn/dynamic/programming/UglyNumbersNaive.java
@@ -0,0 +1,43 @@
+package com.sherlock.learn.dynamic.programming;
+
+import com.google.common.base.Stopwatch;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+//
+public class UglyNumbersNaive
+{
+    public static void main( String[] args )
+    {
+        Stopwatch sw = Stopwatch.createStarted();
+        int i =1500;
+        int j =1;
+        while(i!=0){
+            if(isUgly(j)) {
+                System.out.println(j);
+                i--;
+            }
+            j++;
+        }
+        sw.stop();
+        System.out.println("Time:"+sw.elapsed(MILLISECONDS));
+    }
+
+    static int maxDivide(int a, int b)
+    {
+        while(a % b == 0)
+            a = a/b;
+        return a;
+    }
+
+    /* Function to check if a number
+    is ugly or not */
+    static boolean isUgly(int no)
+    {
+        no = maxDivide(no, 2);
+        no = maxDivide(no, 3);
+        no = maxDivide(no, 5);
+
+        return no == 1;
+    }
+}
